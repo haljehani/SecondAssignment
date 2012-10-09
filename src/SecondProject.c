3c81af1f98bb4302af9a7beeb87afb0e0a6f9bb9
MacBook <macbook@APPLEs-MacBook-Pro-2.local>
2012-10-09 16:33:56 +0300

    done. after creating the header file

--- src/SecondProject.c
+++ src/SecondProject.c
@@ -2,21 +2,7 @@
  *        Matrix Multiplication Using Pthread       *
  ****************************************************/ 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <time.h>
-
-#define TYPE float
-
-void initrandom(void);
-void generateMatrix(TYPE **A, int rA, int cA);
-void mallocMatrix(TYPE ***A, int rA, int cA);
-void prnMatrix(TYPE **A, int rA, int cA);
-void Mulitiplication(TYPE **A, TYPE **B, TYPE **C, int rA,int rB, int cB);
-TYPE rnd_int(int i);
+#include "SecondProject.h"
 
 
 /**************************************************************************************/
