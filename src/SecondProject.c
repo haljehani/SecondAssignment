499c96b1839afa9a5c973b8389b0b82b4ffe1b81
MacBook <macbook@APPLEs-MacBook-Pro-2.local>
2012-10-09 16:02:56 +0300

    tha comments have been added

--- src/SecondProject.c
+++ src/SecondProject.c
@@ -34,7 +34,7 @@ int main(int argc, char * argv[])
 	
 	TYPE **A, **B, **C;
 	
-	
+	// Take the Argument from the Terminal
 	rA=atoi(argv[1]);
 	cA=atoi(argv[2]);
 	rB=atoi(argv[3]);
@@ -52,21 +52,21 @@ int main(int argc, char * argv[])
 	//********************************************
 	
     
-	
+	// Call Function to Allocate Memory
     mallocMatrix(&A, rA, cA);
     mallocMatrix(&B, rB, cB);
     mallocMatrix(&C, rA, cB);
 	
-	
+	// Call Function to Initialize random generator
 	initrandom();
 	
-    
+    // Call Function to Generate matrices
 	generateMatrix(A, rA, cA);
     generateMatrix(B, rB, cB);
 	
 	
 	gettimeofday(&start,NULL);
-	
+	// For loop for rows start from 0 until rA
 	
 	Mulitiplication(A, B, C, rA, rB, cB);
 	
@@ -75,12 +75,12 @@ int main(int argc, char * argv[])
 	elapsed = 1000000 * (stop.tv_sec - start.tv_sec);
 	elapsed += stop.tv_usec - start.tv_usec;
 	
-	
+	// time in usecs
 	printf ("Time: %lu;\t", elapsed);
-	
+	// performance in MFLOPS
 	printf("performance: %lu\n", (unsigned long)((((TYPE)rA)*((TYPE)cA)*((TYPE)cB)*2)/elapsed));
 	
-	
+	// Call Function to Print matrice C
 	prnMatrix(C, rA, cB);
 	
 	//free(A);
@@ -93,7 +93,7 @@ int main(int argc, char * argv[])
 
 
 //--------------------------------------------------------------
-
+// Function to Allocate Memory
 void mallocMatrix(TYPE ***A, int rA, int cA)
 {
     int z;
@@ -105,13 +105,13 @@ void mallocMatrix(TYPE ***A, int rA, int cA)
 
 
 //--------------------------------------------------------------
-
+// Function to Initialize random generator
 void initrandom(void)
 {
-    
+    //Variables
     time_t ltime;
 	
-    
+    //Code of the Function
     time(&ltime);
     ltime%=10000;
     srand(ltime);
@@ -120,27 +120,29 @@ void initrandom(void)
 
 
 //--------------------------------------------------------------
-
+// Function Generation of matrices
 void generateMatrix(TYPE **A, int rA, int cA)
 {
     int i, j;
     
     for(i=0; i<rA; i++)
         for(j=0;j<cA;j++)
-			A[i][j]=rnd_int(100);	
+			A[i][j]=rnd_int(100);	// Random number from 0 to 100
 	//A[i][j]=1.5;
 }
 //--------------------------------------------------------------
 
 
 //--------------------------------------------------------------
-
+// Function Generate random numbers
 TYPE rnd_int(int i)
 {
+	//Variables
     TYPE num;
     TYPE denum = RAND_MAX+1.0;
     TYPE res;
 	
+	//Code of the Function
     num = (TYPE) rand();
     res =(TYPE) i*(num/denum);
     return(res);
@@ -149,13 +151,13 @@ TYPE rnd_int(int i)
 
 
 //--------------------------------------------------------------
-
+// Function to Print matrices
 void prnMatrix(TYPE **A, int rA, int cA)
 {
+    //Variables
+    int i,j;  // counters
     
-    int i,j;  
-    
-    
+    //Code of the Function
     for (i=0; i<rA; i++)
     {
         for (j=0; j<cA; j++)
@@ -172,7 +174,7 @@ void prnMatrix(TYPE **A, int rA, int cA)
 //--------------------------------------------------------------
 void Mulitiplication(TYPE **A, TYPE ** B, TYPE ** C, int rA, int rB, int cB)
 {
-    
+    //Variables
     int i,j,k;
 	TYPE result;
     //result=0.0;
